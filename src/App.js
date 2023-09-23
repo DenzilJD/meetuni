d0780269d1e8820be6c8b5d3e77b225258c052be
Denzil Jude Daniels <denziljudedaniels@gmail.com>
2023-09-23 08:15:00 +0530

    Final Draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Box, Text } from '@chakra-ui/react';
+import { ChatBox } from './ChatBox';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [home, setHome] = useState(true);
+  return <Box
+    bgColor='rgb(30,30,30)'
+    minH='100vh'
+    display='flex'
+    flexDir={home ? 'column' : 'row'}
+    alignItems='center'
+  >
+    <Box display='flex' flexDir='column' alignItems='center' marginLeft={home ? '' : '45px'}>
+      <Text color='white' fontSize={home ? '8rem' : '6rem'} marginTop={home ? '17.5%' : '-20%'}>Chatter</Text>
+      <Text color='gray' fontSize='2rem'>The solution to 'most' of your problems ;)</Text>
+    </Box>
+    <Box
+      className='button'
+      color='white'
+      bgColor='gray'
+      fontSize='3rem'
+      marginTop='5%'
+      borderRadius='100px'
+      padding='5px 17px'
+      paddingBottom='12px'
+      display={home ? '' : 'none'}
+      _hover={{ cursor: 'pointer', color: 'grey', bgColor: 'white' }}
+      onClick={() => setHome(false)}
+    >
+      Try Chatter
+    </Box>
+    {!home && <ChatBox setHome={setHome} />}
+  </Box>
 }
 
 export default App;
+
+
+
+
+// import { Box } from "./Box";
+
+// const [green, setGreen] = useState([]);
+// const [val, setVal] = useState(false);
+// let arr1 = [0, 1, 2], arr2 = [3, 4, 5], arr3 = [6, 7, 8];
+
+// const change = (num) => {
+//   if (!green.includes(num)) {
+//     if (green.length === 8)
+//       setVal(true);
+//     setGreen([...green, num]);
+//   }
+// }
+
+// return <div className="App">
+//   <div>
+//     {arr1.map(i => <Box key={i} i={i} change={change} val={val} green={green} setGreen={setGreen} />)}
+//   </div>
+//   <div>
+//     {arr2.map(i => <Box key={i} i={i} change={change} val={val} green={green} setGreen={setGreen} />)}
+//   </div>
+//   <div>
+//     {arr3.map(i => <Box key={i} i={i} change={change} val={val} green={green} setGreen={setGreen} />)}
+//   </div>
+// </div>
---
+++ src/ChatBox.js
@@ -0,0 +1,190 @@
+import { Box, Button, Input, Spinner, Text, Tooltip, useToast } from '@chakra-ui/react';
+import React, { useState } from 'react';
+import OpenAI from 'openai';
+import { IoMdSend } from 'react-icons/io';
+import { MdDelete } from 'react-icons/md';
+import { BiArrowBack } from 'react-icons/bi';
+
+const OPENAI_API_KEY = '';
+
+export const ChatBox = ({ setHome }) => {
+    const toast = useToast();
+    const [latest, setLatest] = useState();
+    const [loading, setLoading] = useState();
+    const [temp, setTemp] = useState([]);
+
+    const openai = new OpenAI({
+        apiKey: OPENAI_API_KEY,
+        dangerouslyAllowBrowser: true
+    });
+
+    const submitHandler = async () => {
+        if (!latest)
+            return;
+        setLoading(true);
+        try {
+            const chatCompletion = await openai.chat.completions.create({
+                model: "gpt-3.5-turbo",
+                messages: [...temp, { "role": "user", "content": `${latest}` }]
+            });
+            setTemp([...temp, { "role": "user", "content": `${latest}` }, { "role": "assistant", "content": `${chatCompletion.choices[0].message.content}` }])
+            console.log(chatCompletion.choices[0].message.content);
+        }
+        catch (error) {
+            toast({
+                title: "Error!",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "top"
+            });
+        }
+        setLoading(false);
+    }
+
+    return <Box
+        marginLeft='auto'
+        marginRight='5%'
+        width='40%'
+        borderRadius='50px'
+        height='90vh'
+        bgColor='black'
+        color='white'
+        display='flex'
+        flexDir='column'
+    >
+        <Box display='flex'>
+            <Tooltip label='Back' color='black' bgColor='white' borderRadius='100px' padding='2px 5px'>
+                <Button
+                    margin='15px'
+                    marginRight='auto'
+                    border={0}
+                    bgColor='rgb(30,30,30)'
+                    borderRadius='100px'
+                    width='5vw'
+                    h='5vw'
+                    onClick={() => setHome(true)}
+                    _hover={{ cursor: 'pointer' }}
+                >
+                    <BiArrowBack size='40px' color='white' />
+                </Button>
+            </Tooltip>
+            <Tooltip label='Delete Session History' color='white' bgColor='red' borderRadius='100px' padding='2px 5px'>
+                <Button
+                    margin='15px'
+                    border={0}
+                    bgColor='rgb(30,30,30)'
+                    borderRadius='100px'
+                    width='5vw'
+                    h='5vw'
+                    marginLeft='auto'
+                    onClick={() => setTemp([])}
+                    _hover={{ cursor: 'pointer' }}
+                >
+                    <MdDelete size='40px' color='white' />
+                </Button>
+            </Tooltip>
+        </Box>
+        <Box
+            padding='10px 15px'
+            overflow='scroll'
+            css={{
+                '&::-webkit-scrollbar': {
+                    width: '4px'
+                },
+                '&::-webkit-scrollbar-track': {
+                    width: '6px'
+                },
+                '&::-webkit-scrollbar-thumb': {
+                    background: 'transparent',
+                    borderRadius: '24px'
+                }
+            }}
+            _hover={{
+                '&::-webkit-scrollbar-thumb': {
+                    background: '#189AB4',
+                    borderRadius: '24px'
+                }
+            }}
+            display='flex'
+            flexDir='column'
+        >
+            {temp && temp.map(t => {
+                return <Text
+                    bgColor={t.role === 'user' ? 'rgb(17, 161, 113)' : 'rgb(85, 80, 80)'}
+                    marginLeft={t.role === 'user' ? 'auto' : ''}
+                    maxWidth='60%'
+                    borderRadius='20px'
+                    padding='5px'
+                >
+                    {t.content}
+                </Text>
+            })}
+            {loading ? <>
+                <Spinner thickness='4px'
+                    speed='0.65s'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl' /><Text
+                        bgColor='rgb(17, 161, 113)'
+                        marginLeft='auto'
+                        maxWidth='60%'
+                        borderRadius='20px'
+                        padding='5px'
+                    >{latest}</Text>
+            </> : ''}
+        </Box>
+        <Box marginTop='auto' display='flex' padding='15px'>
+            <Input variant='flushed'
+                placeholder='Chat with Chatter'
+                letterSpacing={0.01}
+                _placeholder={{ fontSize: '2rem' }}
+                fontSize='2rem'
+                height='3rem'
+                padding='15px'
+                width='85%'
+                type='text'
+                outline='none'
+                border={0}
+                borderLeftRadius='100px'
+                bgColor='rgb(30,30,30)'
+                color='white'
+                onChange={(e) => setLatest(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter')
+                        submitHandler();
+                }}
+            />
+            <Button
+                border={0}
+                bgColor='rgb(30,30,30)'
+                borderRightRadius='100px'
+                width='15%'
+                marginLeft='auto'
+                onClick={submitHandler}
+                _hover={{ cursor: 'pointer' }}
+            >
+                <IoMdSend size='40px' color='white' />
+            </Button>
+        </Box>
+    </Box>
+}
+
+
+// export const Box = ({ i, change, val, green, setGreen }) => {
+//     const [col, setCol] = useState('');
+//     if (green.length > 0);
+//     console.log(green[0], " ", i);
+//     setTimeout(() => {
+//         if (val && green.length > 0 && green[0] === i) {
+//             setCol('orange');
+//             setGreen(temp => temp.filter(j => j !== i));
+//         }
+//     }, 1000);
+
+//     return <div id={i} className='Box' style={{ backgroundColor: col }} onClick={() => {
+//         setCol('green');
+//         if (col === '')
+//             change(i);
+//     }} />
+// }
